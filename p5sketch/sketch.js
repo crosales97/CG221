f27fa26bb27dac1fa7e704b1173e8f0bab282b91
crosales97 <casey.m.rosales@gmail.com>
2017-04-26 21:21:48 -0400

    Update sketch.js

--- p5sketch/sketch.js
+++ p5sketch/sketch.js
@@ -1,13 +1,19 @@
 var party = 100;
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(700,700);
   background(0);
 }
 function draw() {
-  fill('#');
-  ellipse(200,200);
-  fill('#');
-  rect(100,100,);
-
+  background(250);
+  
+  circle(100,100,party,party);
+  
+  translate(240, 0, 0);
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(70, 70, 70);
+  pop();
 }
