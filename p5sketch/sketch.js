db83c18f56d9662797da5c0bd86cd07ecf4a88bd
crosales97 <casey.m.rosales@gmail.com>
2017-05-03 19:22:19 -0400

    Update sketch.js

--- p5sketch/sketch.js
+++ p5sketch/sketch.js
@@ -1,13 +1,10 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(640, 480, WEBGL);
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw(){
+  background(200);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(200, 200, 200);
 }
-
