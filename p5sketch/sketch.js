d02f88a497a49c783fdc07db8b5402487a80ed6d
crosales97 <casey.m.rosales@gmail.com>
2017-05-03 19:15:08 -0400

    Update sketch.js

--- p5sketch/sketch.js
+++ p5sketch/sketch.js
@@ -1,19 +1,13 @@
-var party = 100;
-
 function setup() {
-  createCanvas(700,700);
-  background(0);
+  createCanvas(640, 480);
 }
+
 function draw() {
-  background(250);
-  
-  circle(100,100,party,party);
-  
-  translate(240, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(70, 70, 70);
-  pop();
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
+
