5dfcc79f70a5e5192a9efdb0d52c4b921a997ae6
crosales97 <casey.m.rosales@gmail.com>
2017-05-03 21:02:29 -0400

    Update sketch.js

--- p5sketch/sketch.js
+++ p5sketch/sketch.js
@@ -1,10 +1,21 @@
 
 function setup() {
-  createCanvas(640, 480, WEBGL);
+  createCanvas(1000, 1000, WEBGL);
 }
 
 function draw(){
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
   box(100, 100, 100);
+  translate(100,100,-100);
+  sphere(50);
+  translate(-100,100,100);
+  sphere(50);
+
+  beginShape();
+vertex(100,23,-100);
+vertex(200,23,-50);
+vertex(150, 45,-100);
+vertex(300,50,-50)
+endShape();
 }
