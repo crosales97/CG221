9f23ce6aaa074b31a744a856e6485eab49095dea
crosales97 <casey.m.rosales@gmail.com>
2017-05-03 20:33:47 -0400

    Update sketch.js

--- p5sketch/sketch.js
+++ p5sketch/sketch.js
@@ -1,10 +1,10 @@
+
 function setup() {
   createCanvas(640, 480, WEBGL);
 }
 
 function draw(){
-  background(200);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  box(200, 200, 200);
+  box(100, 100, 100);
 }
