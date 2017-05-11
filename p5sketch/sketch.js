ff5792a16d4404110bf215f4afcf2aaa3473e10e
crosales97 <casey.m.rosales@gmail.com>
2017-05-10 20:10:18 -0400

    Update sketch.js

--- p5sketch/sketch.js
+++ p5sketch/sketch.js
@@ -1,14 +1,19 @@
 
 function setup() {
-  createCanvas(1000, 1000, WEBGL);
+  createCanvas(1500, 1000, WEBGL);
 }
 
 function draw(){
+  push();
+
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
   box(100, 100, 100);
+
   translate(100,100,-100);
   sphere(50);
+
+
   translate(-100,100,100);
   sphere(50);
 
@@ -18,4 +23,40 @@ vertex(200,23,-50);
 vertex(150, 45,-100);
 vertex(300,50,-50)
 endShape();
+
+pop();
+
+
+
+  push();
+translate(0, 0, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70, 20);
+  pop();
+
+push();
+if (mouseIsPressed)
+translate(pmouseX, pmouseY)
+else
+translate(240, 0, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(70, 20);
+  pop();
+
+  push();
+  translate(-240, 0, 0);
+    rotateZ(frameCount * 0.01);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    torus(70, 20);
+    pop();
+
+    push();
+    translate(mouseX, mouseY, pmouseX, pmouseY);
+    sphere(50);
+    pop();
 }
